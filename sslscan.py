fb7f3f98a4c934f9a860b3c4cd27eb46b4c9f777
Andrew Dunham <andrew@du.nham.ca>
2014-02-24 20:32:27 -0800

    Add more documentation, refactor code a bit

--- sslscan.py
+++ sslscan.py
@@ -13,7 +13,14 @@ except ImportError:
     from http_parser.pyparser import HttpParser
 
 
+__author__ = 'Andrew Dunham <andrew@du.nham.ca>'
+__version__ = '0.0.1'
+
+
 def get_all_ciphers(method):
+    """
+    Get all ciphers supported by this version of OpenSSL.
+    """
     ssl_method = getattr(SSL, method.replace('.', '_') + '_METHOD')
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -30,15 +37,19 @@ def get_all_ciphers(method):
 
 
 def make_request(sock, server_name):
+    """
+    Given an open socket, makes a simple HTTP request, parses the response, and
+    returns a dictionary containing the HTTP headers that were returned by the
+    server.
+    """
     p = HttpParser()
 
     request = ('GET / HTTP/1.0\r\n' +
-               'User-Agent: SSLScan\r\n' +
+               'User-Agent: pySSLScan\r\n' +
                'Host: %s\r\n\r\n' % (server_name,))
-    sock.write(request)
+    sock.write(request.encode('ascii'))
 
     headers = None
-    begin_done = False
     while True:
         data = sock.recv(1024)
         if not data:
@@ -48,17 +59,17 @@ def make_request(sock, server_name):
         nparsed = p.execute(data, recved)
         assert nparsed == recved
 
-        if p.is_message_begin() and not begin_done:
-            begin_done = True
-
         if p.is_headers_complete():
             headers = p.get_headers()
             break
 
-    return begin_done, headers
+    return headers
 
 
-def test_cipher(server, port, method, cipher):
+def test_single_cipher(server, port, method, cipher):
+    """
+    Test to see if the server supports a given method/cipher combination.
+    """
     ssl_method = getattr(SSL, method.replace('.', '_') + '_METHOD')
     context = SSL.Context(ssl_method)
     context.set_cipher_list(cipher)
@@ -74,7 +85,29 @@ def test_cipher(server, port, method, cipher):
             print('Accepted\t' + method + '\t' + cipher)
     except SSL.Error as e:
         print('Failed\t' + method + '\t' + cipher)
-        x, y = e.args[0]
+    finally:
+        sock.close()
+
+
+def test_preferred_cipher(server, port, method):
+    """
+    Test what the server's preferred cipher is when a client will accept all
+    ciphers.
+    """
+    ssl_method = getattr(SSL, method.replace('.', '_') + '_METHOD')
+    context = SSL.Context(ssl_method)
+    context.set_cipher_list("ALL:COMPLEMENTOFALL")
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock = SSL.Connection(context, sock)
+        sock.connect((server, port))
+
+        headers = make_request(sock, server)
+
+        # TODO: extract the actual cipher in use
+    except SSL.Error as e:
+        pass
     finally:
         sock.close()
 
@@ -90,14 +123,25 @@ def main():
     # TODO: get this from the user
     threads = 5
 
-    for method in ['SSLv3', 'TLSv1']:
-        supported_ciphers = get_all_ciphers(method)
+    ssl_version = SSL.SSLeay_version(SSL.SSLEAY_VERSION)
+    if not isinstance(ssl_version, str):
+        ssl_version = ssl_version.decode('ascii')
+    print("pySSLScan version %s (%s)" % (
+        __version__, ssl_version
+    ))
 
-        with futures.ThreadPoolExecutor(max_workers=threads) as executor:
-            res = executor.map(lambda cipher: test_cipher(server, port, method, cipher),
-                               supported_ciphers)
-            for x in res:
-                pass
+    with futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        results = []
+
+        for method in ['SSLv3', 'TLSv1']:
+            supported_ciphers = get_all_ciphers(method)
+
+            # Test each individual cipher.
+            for cipher in supported_ciphers:
+                results.append(executor.submit(test_single_cipher, server, port, method, cipher))
+
+            # Test for the preferred cipher suite for this method.
+            results.append(executor.submit(test_preferred_cipher, server, port, method))
 
 
 if __name__ == "__main__":
